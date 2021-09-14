71827fb93609292bd68d22b43cd3afa1eb83b3fb
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-09-14 15:37:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import PyPDF2
-import Split
 from subprocess import call
+import sys
 
+#split function
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import os, errno
+
+#google translate
 from googletrans import Translator, constants
 from pprint import pprint
 
-filename = 'pdf.pdf'
+def split(directory, filename):
+    inputpdf = PdfFileReader(open(filename, "rb"))
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    for i in range(inputpdf.numPages):
+        output = PdfFileWriter()
+        output.addPage(inputpdf.getPage(i))
+        with open(directory+ "/%s.pdf" % i, "wb") as outputStream:
+            output.write(outputStream)
+
+#define pdf file
+filename = '/home/ubuntu/Desktop/pdf.pdf'
 directory = "splitted/" + filename
 
-Split.split(directory, filename)
+split(directory, filename)
+
 pdfFileObj = open(filename, 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
@@ -21,10 +42,9 @@ for i in range(pdfReader.numPages):
     translator = Translator()
 
     translation = translator.translate(data, src="en", dest="ru")
-    
-    text = translation.text
+    print(translation)
+      
 
-    with open('test.txt','a') as f:
-        f.write(text)
-
+if __name__ == "__main__":
+    split(directory, filename)
 
